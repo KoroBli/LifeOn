86b07c97d792d14e4e60c22c61db4284558b957b
KoroBli <jjorba95@gmail.com>
2020-10-09 18:52:19 +0200

    Created Rainy PS & corrected day/night script
    Created some prefabs for rainy PSand day/night cycle

---
+++ LifeOn/Assets/Scripts/WorldScripts/DayNightCycle/DayNightCycle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public float timeSpeed;
+
+    public Transform sun;
+    public Transform moon;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        sun.RotateAround(sun.transform.position, Vector3.right, timeSpeed * Time.deltaTime);
+        moon.RotateAround(moon.transform.position, Vector3.right, timeSpeed * Time.deltaTime);
+    }
+}
